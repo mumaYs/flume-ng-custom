04485e37177186cb08c19e1d011373bfaa97db4a
muma <18551626906@163.com>
2017-10-13 15:45:20 +0800

    新建flume开发程序

--- src/main/java/com/ccfsoft/flume/interceptor/HdfsInterceptor.java
+++ src/main/java/com/ccfsoft/flume/interceptor/HdfsInterceptor.java
@@ -1,4 +1,6 @@
 package com.ccfsoft.flume.interceptor;
+import com.alibaba.fastjson.JSONObject;
+import com.ccfsoft.utils.PropertyConstants;
 import com.google.common.collect.Lists;
 import org.apache.flume.Context;
 import org.apache.flume.Event;
@@ -6,7 +8,11 @@ import org.apache.flume.interceptor.Interceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 映射数据类型和Hdfs路径的拦截器
@@ -19,48 +25,44 @@ public class HdfsInterceptor implements
     static final String EXTRACTOR_HEADER_KEY = "extractorHeaderKey";
     private final String extractorHeaderKey;
 
+    /** hdfs目录映射*/
+    private static Map<String,String> hdfsMap = new HashMap<>();
+
     private HdfsInterceptor(String extractorHeaderKey) {
         this.extractorHeaderKey = extractorHeaderKey;
     }
 
+
     /**
-     * 初始化加载数据源种类和HDFS目录的对应关系
+     * 初始化加载hdfs映射
      */
     public void initialize() {
-
+        Iterator<String> it= PropertyConstants.getProperties().stringPropertyNames().iterator();
+        while(it.hasNext()){
+            String key=it.next();
+            hdfsMap.put(key,PropertyConstants.getPropertiesKey(key));
+        }
     }
 
-    public void close() {
-        // NO-OP...
-    }
+    public void close() { }
 
     public Event intercept(Event event) {
-        String tmpHeadStr;
         String body = null;
-//        //获取头信息,做数据分类
-//        Map<String, String> headers = event.getHeaders();
-//        tmpHeadStr = headers.get(extractorHeaderKey);
-//
-//        if ("H3C".equals(tmpHeadStr)) {
-//            body = getH3CReport(event.getBody());
-//        } else if ("SKSPRUCE".equals(tmpHeadStr)) {
-//            body = getSkspruceReport(event.getBody());
-//        }
-
-//        try {
-//            //直接通过数据格式分类
-//            body = BillParse.billParse(new String(event.getBody(),"UTF-8"));
-//        } catch (UnsupportedEncodingException e) {
-//            e.printStackTrace();
-//        }
-//
-//        if (body == null) {
-//            return null;
-//        } else {
-////            logger.info(String.format(
-////                    "结果:%s", body));
-//            event.setBody(body.getBytes());
-//        }
+        try {
+            //解析数据内容和数据种类: 事件{ "data":"数据内容" , "datatype":"数据类型" }
+            body = new String(event.getBody(),"UTF-8");
+            logger.info(String.format(
+                    "结果:%s", body));
+            Map eventBody = JSONObject.parseObject(body);
+            Map<String,String> head = new HashMap<>();
+            String dataType = eventBody.get("datatype").toString();
+            head.put("dataType",dataType);
+            head.put("hdfsDir",hdfsMap.get(dataType));
+            event.setHeaders(head);
+            event.setBody(eventBody.get("data").toString().getBytes());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         return event;
     }
 
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,14 +1,38 @@
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.ccfsoft.utils.PropertyConstants;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by muma on 2017.3.1.
  */
 public class Test {
+    private static Map<String,String> hdfsMap = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
-        String eventBody = "2015-12-31 00:39:02|0|4600318154981212|18154981212|13627551145|2015-12-31|00:39:01|2015-12-31|00:42:27|204|||||||||||||460001583|31.892155661086584|118.57032141470845| ";
-        String[] bodys = eventBody.split("\\|");
-        System.out.print(bodys.length);
+//        String eventBody = "2015-12-31 00:39:02|0|4600318154981212|18154981212|13627551145|2015-12-31|00:39:01|2015-12-31|00:42:27|204|||||||||||||460001583|31.892155661086584|118.57032141470845| ";
+//        String[] bodys = eventBody.split("\\|");
+//        System.out.print(bodys.length);
+        Iterator<String> it= PropertyConstants.getProperties().stringPropertyNames().iterator();
+        while(it.hasNext()){
+            String key=it.next();
+            hdfsMap.put(key,PropertyConstants.getPropertiesKey(key));
+        }
+        String jsonStr = "{ \"data\":\"数据内容\" , \"datatype\":\"数据类型\" }";
+
+
+        Map eventBody = JSONObject.parseObject(jsonStr);
+        Map<String,String> head = new HashMap<>();
+        String dataType = eventBody.get("datatype").toString();
+        head.put("dataType",dataType);
+        head.put("hdfsDir",hdfsMap.get(dataType));
+
+
+//        for(java.util.Map.Entry<String,Object> entry: JSONObject.parseObject(jsonStr).entrySet()){
+//            System.out.print(entry.getKey()+"-"+entry.getValue()+"\t");
+//        }
     }
 }
